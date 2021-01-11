17db7165552e9535a694b18be70305cf51650273
SLane35 <SLane35@users.noreply.github.com>
2021-01-11 21:44:10 +0000

    Add files via upload

--- starter_file/score.py
+++ starter_file/score.py
@@ -5,15 +5,15 @@ import joblib
 
 def init():
     global model
-    model_path = os.path.join(os.getenv('myenv'), 'automlmodel.pkl')
-    model = joblib.load('automl_pred')
+    model_path = os.path.join(os.getenv('AZUREML_MODEL_DIR'), 'automlmodel.pkl')
+    model = joblib.load(model_path)
 
-    def run(data):
-        try:
-            data = np.array(json.loads(data))
-            result = model.predict(data)
-            # You can return any data type, as long as it is JSON serializable.
-            return result.tolist()
-        except Exception as e:
-            error = str(e)
-            return error
+def run(data):
+    try:
+        data = np.array(json.loads(data))
+        result = model.predict(data)
+        # You can return any data type, as long as it is JSON serializable.
+        return result.tolist()
+    except Exception as e:
+        error = str(e)
+        return error
